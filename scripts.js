8e0390052a3b6d9fa34f742c92bacbf8ebfc04d4
lipika202 <libikalibika3@gmail.com>
2024-12-21 09:27:21 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// JavaScript to toggle the menu on smaller screens
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+  }
+  
